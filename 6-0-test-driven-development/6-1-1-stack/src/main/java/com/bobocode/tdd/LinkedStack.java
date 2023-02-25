acd3f16c15b28db1145de598b6a437c00379b0dc
dykov <dykovoleksii@gmail.com>
2023-02-25 19:59:19 +0200

    Add 6-1-1-stack solution

--- 6-0-test-driven-development/6-1-1-stack/src/main/java/com/bobocode/tdd/LinkedStack.java
+++ 6-0-test-driven-development/6-1-1-stack/src/main/java/com/bobocode/tdd/LinkedStack.java
@@ -3,23 +3,50 @@ package com.bobocode.tdd;
 import com.bobocode.util.ExerciseNotCompletedException;
 
 public class LinkedStack<T> implements Stack<T> {
+    private class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(final T value) {
+            this.value = value;
+        }
+
+        public Node(final T value, final Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Node<T> head;
+    private int size;
+
     @Override
     public void push(T element) {
-        throw new ExerciseNotCompletedException(); // todo
+        final Node<T> newNode = new Node<>(element);
+        if(head != null) {
+            newNode.next = head;
+        }
+        head = newNode;
+        size++;
     }
     
     @Override
     public T pop() {
-        throw new ExerciseNotCompletedException(); // todo
+        final Node<T> nodeToReturn = head;
+        if(head != null) {
+            head = head.next;
+            size--;
+        }
+        return nodeToReturn == null ? null : nodeToReturn.value;
     }
 
     @Override
     public int size() {
-        throw new ExerciseNotCompletedException(); // todo
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new ExerciseNotCompletedException(); // todo
+        return head == null;
     }
 }
